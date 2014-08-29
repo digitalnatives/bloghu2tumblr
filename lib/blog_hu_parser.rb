b09d3926bc262bb4cf878a626fbecf2e2ae6f5fb
boni <boni@twine.hu>
2014-08-29 11:01:53 +0200

    read file

--- lib/blog_hu_parser.rb
+++ lib/blog_hu_parser.rb
@@ -1,8 +1,25 @@
+require 'nokogiri'
+
 class BlogHuParser
 
   attr_accessor :posts
+  attr_reader :xml_file
 
-  def initialize(xml_file) 
-
+  def initialize(file_name)
+    @posts = []
+    @xml_file = load_file(file_name)
+    @xml_file.nil? ? puts('No file') : parse
   end
+
+  def parse
+  end
+
+  private
+
+  def load_file(file_name)
+    File.read(file_name)
+  rescue Errno::ENOENT => e
+    puts "No file"
+  end
+
 end
