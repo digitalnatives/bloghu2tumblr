4fe78dd842aff7c1eea13aa4b6a808f9ff2ab5f0
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2014-08-29 10:40:30 +0200

    Basic post creation

--- lib/post.rb
+++ lib/post.rb
@@ -1,11 +1,35 @@
 class Post
-  attr_accessor :type   # String [text, photo, quote, link, chat, audio, video]
-  attr_accessor :state  # String [published, draft, queue, private]
-  attr_accessor :tags   # Array  [Comma-separated tags for this post]
-  attr_accessor :tweet  # String [off to disable auto tweeting]
-  attr_accessor :date   # String [The GMT date and time of the post, as a string]
-  attr_accessor :format # String [html, markdown]
+  ATTRIBUTES = [:state, :tags, :tweet, :date, :format, :slug, :title, :body]
+  attr_accessor :type   # String  [text, photo, quote, link, chat, audio, video]
+  attr_accessor :state  # String  [published, draft, queue, private]
+  attr_accessor :tags   # Array   [Comma-separated tags for this post]
+  attr_accessor :tweet  # Boolean [off to disable auto tweeting]
+  attr_accessor :date   # Time    [The GMT date and time of the post, as a string]
+  attr_accessor :format # String  [html, markdown]
   attr_accessor :slug   # String
   attr_accessor :title  # String
   attr_accessor :body   # String
+
+  def to_request_params
+    ATTRIBUTES.each_with_object({}) do |attr, hash|
+      attr_method_name = "#{attr}_to_request_param"
+      attr_method      = respond_to?(attr_method_name) ? attr_method_name : attr
+
+      value = send(attr_method)
+      hash[attr] = value if value
+    end
+  end
+
+  private
+  def tags_to_request_param
+    tags.to_a.join(', ')
+  end
+
+  def tweet_to_request_param
+    !!tweet ? nil : 'off'
+  end
+
+  def date_to_request_param
+    date.to_s
+  end
 end
---
+++ post.rb
@@ -0,0 +1,25 @@
+require 'bundler/setup'
+Bundler.require
+require_relative './lib/post'
+
+Tumblr.configure do |config|
+  config.consumer_key       = ENV['CONSUMER_KEY']
+  config.consumer_secret    = ENV['CONSUMER_SECRET']
+  config.oauth_token        = ENV['OAUTH_TOKEN']
+  config.oauth_token_secret = ENV['OAUTH_TOKEN_SECRET']
+end
+
+post = Post.new.tap do |p|
+  p.type  = 'text'
+  p.state = 'published'
+  p.tags  = ['one', 'two', 'three']
+  p.tweet = false
+  p.date  = Time.new(2014, 7, 30, 2, 2, 2, '+02:00')
+  p.format = 'html'
+  p.slug = 'whatever'
+  p.title = 'Something'
+  p.body = 'Text <br> and a <p>one</p> <marquee>blink and scroll</marquee>'
+end
+
+client = Tumblr::Client.new
+client.create_post(:text, 'dnfakeblog.tumblr.com', post.to_request_params)
